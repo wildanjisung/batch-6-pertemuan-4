a3a631cf5f4c79f420d14d5bfd18f8107a5d24b2
wildanjisung <wildanjisung@gmail.com>
2023-09-25 20:52:21 +0700

    add props and emit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ pesan }}</h1>
+    <button @click='increment'>tambah</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +35,12 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    pesan: String
+  },
+  methods: {
+    increment() {
+      this.$emit('update-ajah')
+    }
   }
 }
 </script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloWorld pesan="Welcome to About Page" @update-ajah='munculinTulisan'/>
+    <p>nilai: {{ nilai }} </p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      nilai: 0
+    }
+  },
+  methods: {
+    munculinTulisan() {
+      console.log("haii saya tulisan");
+    },
+    doIncrement() {
+      this.nilai++
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log('halaman mounted');
+    console.log('nilai semula: ', this.nilai);
+    this.nilai++
+    console.log('nilai sesudah: ', this.nilai);
+  }
+}
+</script>
